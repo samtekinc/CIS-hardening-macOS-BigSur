015e65dadddd6d4c27028c7621d4f61d024eb386
Zachary Hopp <zacharyhopp@Zacharys-MacBook-Pro.local>
2021-07-01 15:25:44 -0400

    Added 2.2.1, 2.2.2, 2.3.1, 2.3.2

--- bash/2.X/tasks/main.bash
+++ bash/2.X/tasks/main.bash
@@ -12,3 +12,33 @@ for user in $users; do
     echo "Adding BT icon to menu bar for user $user"
     su $user defaults -currentHost write com.apple.controlcenter.plist Bluetooth -int 18
 done
+
+## 2.3.1 Set an inactivity interval of 20 minutes or less for the screen saver 
+ for user in $users; do 
+    echo "Setting idle time for user $users to 20 minutes"
+    su $user defaults -currentHost write com.apple.screensaver idleTime -int 1200
+done
+
+## 2.3.2 Secure screen saver corners
+cornerone= $user defaults read com.apple.dock wvous-tl-corner
+cornertwo= $user defaults read com.apple.dock wvous-bl-corner
+cornerthree= $user defaults read com.apple.dock wvous-tr-corner
+cornerfour= $user defaults read com.apple.dock wvous-br-corner
+corneronefix= $user defaults write com.apple.dock wvous-tl-corner -int 0
+cornertwofix= $user defaults write com.apple.dock wvous-bl-corner -int 0
+cornerthreefix= $user defaults write com.apple.dock wvous-tr-corner -int 0
+cornerfourfix= $user defaults write com.apple.dock wvous-br-corner -int 0 
+
+for user in $users; do
+    if [ $cornerone == 6 ]; then 
+        $corneronefix
+    fi 
+    if [$cornertwo == 6 ]; then
+        $cornertwofix
+    fi
+    if [ $cornerthree == 6 ]; then
+        $cornerthreefix
+    fi
+    if [ $cornerfour == 6 ]; then
+        $cornerfourfix
+    fi
