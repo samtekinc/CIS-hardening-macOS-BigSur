dbb4a39ee6982f2b062d348c2d3f3ef32737030f
Zachary Hopp <zacharyhopp@Zacharys-MacBook-Pro.local>
2021-07-01 14:07:26 -0400

    Added 2.1.2

--- bash/2.X/tasks/main.bash
+++ bash/2.X/tasks/main.bash
@@ -1,3 +1,4 @@
+#!/bin/bash
 ## 2.1.1 Turn off Bluetooth, if no paired devices exist
 bt_power=$(defaults read /Library/Preferences/com.apple.Bluetooth ControllerPowerState)
 bt_datatype=$(system_profiler SPBluetoothDataType | grep "Bluetooth:" -A 20 | grep Connectable)
@@ -6,4 +7,8 @@ if [[ $bt_power == 1 && $bt_datatype == "Connectable: No." ]]; then
 fi
 
 ## 2.1.2 Show Bluetooth status in menu bar 
-sudo -u <username> defaults -currentHost write com.apple.controlcenter.plist Bluetooth -int 18
+users=$(dscl . list /Users | grep -v '^_' | grep -v 'daemon' | grep -v 'nobody' | grep -v 'root')
+for user in $users; do
+    echo "Adding BT icon to menu bar for user $user"
+    su $user defaults -currentHost write com.apple.controlcenter.plist Bluetooth -int 18
+done
