3f78770f3f70b5855babb6c54ab12575b9eaf800
Zachary Hopp <zacharyhopp@zacharys-mbp.myfiosgateway.com>
2021-07-12 16:09:03 -0400

    Finished 2.X

--- bash/2.X/tasks/main.bash
+++ bash/2.X/tasks/main.bash
@@ -53,3 +53,63 @@ done
 # 2.3.3 Familiarize users with screen lock tools or corner to Start Screen Saver (Manual)
 # This remediation is made in 2.3.2.
 # In this case the bottom right corner's functionality is to start the Screensaver.
+
+# 2.4.7 Disable Bluetooth Sharing
+for user in $users; do
+    su $user
+    defaults -currentHost write com.apple.Bluetooth PrefKeyServicesEnabled -bool false
+done 
+
+# 2.4.12 Ensure AirDrop is Disabled
+for user in $users; do 
+    su $user 
+    defaults write com.apple.NetworkBrowser DisableAirDrop -bool true 
+done 
+
+# 2.5.6 Limit Ad tracking and personalized Ads
+for user in $users; do
+    su $user
+    defaults -currentHost write /Users/$user/Library/Preferences/com.apple.Adlib.plist allowApplePersonalizedAdvertising -bool false
+done 
+# 2.6.1 iCloud configuration is a level 2 remediation 
+
+# 2.6.2 iCloud keychain is a level 2 remediation
+
+# 2.6.3 iCloud Drive is a level 2 remediation
+
+# 2.6.4 iCloud Drive Document and Desktop sync is a level 2 remediation
+
+# 2.7.1 Time Machine Auto-Backup is a level 2 remediation
+
+# 2.7.2 check 
+
+
+
+# 2.10 Enable Secure Keyboard Entry in terminal.app 
+for user in $users; do
+    su $user
+    defaults write -app Terminal SecureKeyboardEntry -bool true
+done
+
+# 2.11 skipped
+
+# 2.12 Automatic Actions for Optical Media
+for user in $users; do
+    su $user
+    media_type=("com.apple.digihub.blank.cd.appeared" "com.apple.digihub.blank.dvd.appeared" "com.apple.digihub.cd.music.appeared" "com.apple.digihub.cd.picture.appeared" "com.apple.digihub.dvd.video.appeared")
+    for i in "${media_type[@]}"; do
+        defaults write /Users/$user/Library/Preferences/com.apple.digihub $i -dict action 1
+    done
+done
+# 2.13 Review Siri Settings
+for user in $users; do
+    su $user
+    defaults write com.apple.assistant.support.plist 'Assistant Enabled' -bool false
+    defaults write com.apple.Siri.plist LockscreenEnabled -bool false
+    defaults write com.apple.Siri.plist StatusMenuVisible -bool false
+    defaults write com.apple.Siri.plist VoiceTriggerUserEnabled -bool false
+    killall -HUP cfprefsd
+    killall SystemUIServer
+done
+# 2.14 Review Sidecar Settings: Sidecar has no known security issues. 
+
